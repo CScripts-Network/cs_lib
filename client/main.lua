e1b44a3d69b8c52b1087e3dbb0006883de3e6d27
Dominik <55330408+Dom-in@users.noreply.github.com>
2023-12-13 22:45:09 +0000

    Added dist for ReplaceNearProp

--- client/main.lua
+++ client/main.lua
@@ -306,8 +306,8 @@ Core = {
         TaskSmartFleePed(ped, GetPlayerPed(PlayerId()), 100.0, -1)
     end,
 
-    ReplaceNearProp = function(ped, new_prop, old_prop)
-        local PropNear = GetClosestObjectOfType(GetEntityCoords(ped), 5.0, GetHashKey(old_prop))
+    ReplaceNearProp = function(ped, dist, new_prop, old_prop)
+        local PropNear = GetClosestObjectOfType(GetEntityCoords(ped), dist, GetHashKey(old_prop))
         if DoesEntityExist(PropNear) then
             CreateModelSwap(GetEntityCoords(PropNear), 0.5, GetHashKey(old_prop), GetHashKey(new_prop), false)
         end
